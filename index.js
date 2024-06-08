7cd66ae473ab142f9bf3bf27516d7cbf2d357411
HaUma-123 <d16662536@gmail.com>
2024-06-09 02:24:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
+const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const openWeatherApiKey = "ce5048473491559fe3db75ee1432b127"; // Replace with your OpenWeatherMap API key
-
-app.use(cors()); // Enable CORS
-
-app.get('/weather', async (req, res) => {
-  const cityName = req.query.city;
-
-  if (!cityName) {
-    return res.status(400).json({ error: 'City parameter is required' });
-  }
-
+app.get('/advice', async (req, res) => {
   try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openWeatherApiKey}&units=metric`);
-    if (response.data.cod !== 200) {
-      return res.status(404).json({ error: response.data.message });
-    }
+    const response = await axios.get('https://api.adviceslip.com/advice');
+    const advice = response.data.slip;
 
-    return res.json(response.data);
-  } catch (err) {
-    const errorMessage = err.response?.data?.message || err.message || 'Unknown error';
-    return res.status(500).json({ error: `An error occurred: ${errorMessage}` });
+    res.json({
+      advice: advice.advice
+    });
+  } catch (error) {
+    const errorMessage = error.response?.data.message || error.message;
+    res.status(500).json({
+      error: 'An error occurred: ' + errorMessage
+    });
   }
 });
 
